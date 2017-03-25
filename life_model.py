7c120e51b24e320060ab25e4fc7a4589ec5386d1
sergmiller <sergey.miller@phystech.edu>
2017-03-25 16:59:19 +0300

    fixed life

--- life_model.py
+++ life_model.py
@@ -25,7 +25,8 @@ class LifeModel(object):
     # constants
     STATS_IMPORTANT_CELLS_TYPES = [CellType.FREE_SPACE, CellType.VICTIM]
     LIVING_CELLS_TYPES = [CellType.VICTIM, CellType.PREDATOR]
-    DEFAULT_LIFE_COUNTER = {CellType.VICTIM : 0, CellType.PREDATOR : 0}
+    DEFAULT_LIFE_COUNTER = {CellType.VICTIM: 0, CellType.PREDATOR: 0}
+    LEFT_SUITABLE_INDEX = 0
 
     # JUST FOR VISUAL STYLE
     COLORS = ('', '0;32;40m', '0;31;40m', '0;30;43m')  # red, green, yellow
@@ -35,17 +36,21 @@ class LifeModel(object):
     CELL_VISUALIZATION_SYMBOL = (' ', '@', '#', ' ')
 
     class Cell(object):
-        def __init__(self, cell_type, time_to_death, time_to_reproduction_for_victims, time_to_reproduction_for_predators):
+        def __init__(
+            self, cell_type, time_to_death,
+                time_to_reproduction_victims, time_to_reproduction_predators):
             self.cell_type = cell_type
             self.time_to_death = time_to_death
-            self.time_to_reproduction_for_victims = time_to_reproduction_for_victims
-            self.time_to_reproduction_for_predators = time_to_reproduction_for_predators
+            self.time_to_reproduction_victims = time_to_reproduction_victims
+            self.time_to_reproduction_predators = time_to_reproduction_predators
             self.time_to_death = time_to_death
             self.change_cell_type(cell_type)
             self.used = False
 
         def reset_time_to_reproduction(self):
-            self.time_to_reproduction = self.time_to_reproduction_for_victims if self.cell_type == LifeModel.CellType.VICTIM else self.time_to_reproduction_for_predators
+            self.time_to_reproduction = self.time_to_reproduction_victims\
+                if self.cell_type == LifeModel.CellType.VICTIM else\
+                self.time_to_reproduction_predators
 
         def change_cell_type(self, cell_type):
             self.cell_type = cell_type
@@ -70,20 +75,21 @@ class LifeModel(object):
             return ' '
 
     def __init__(
-        self, row_number=10, column_number=10, probability_of_victim_cell=0.25,
-            probability_of_predator_cell=0.25, probability_of_wall_cell=0.25, p_vict_move=0.25,
-            p_pred_move=0.5, time_to_death=3, time_to_reproduction_for_victims=5, time_to_reproduction_for_predators=5,
+            self, row_number=10, column_number=10, probability_victim_cell=0.25,
+            probability_predator_cell=0.25, probability_wall_cell=0.25,
+            probability_victim_move=0.25, probability_predator_move=0.5, time_to_death=3,
+            time_to_reproduction_victims=5, time_to_reproduction_predators=5,
             visualize=0, visualisation_pause=0, random_state=None):
         self.row_number = row_number
         self.column_number = column_number
-        self.probability_of_victim_cell = probability_of_victim_cell
-        self.probability_of_predator_cell = probability_of_predator_cell
-        self.probability_of_wall_cell = probability_of_wall_cell
-        self.p_vict_move = p_vict_move
-        self.p_pred_move = p_pred_move
+        self.probability_victim_cell = probability_victim_cell
+        self.probability_predator_cell = probability_predator_cell
+        self.probability_wall_cell = probability_wall_cell
+        self.probability_victim_move = probability_victim_move
+        self.probability_predator_move = probability_predator_move
         self.time_to_death = time_to_death
-        self.time_to_reproduction_for_victims = time_to_reproduction_for_victims
-        self.time_to_reproduction_for_predators = time_to_reproduction_for_predators
+        self.time_to_reproduction_victims = time_to_reproduction_victims
+        self.time_to_reproduction_predators = time_to_reproduction_predators
         self.visualize = visualize
         self.visualisation_pause = visualisation_pause
         self.life_counter = LifeModel.DEFAULT_LIFE_COUNTER
@@ -92,10 +98,17 @@ class LifeModel(object):
         if random_state is not None:
             random.seed(random_state)
 
-        special_random_cell_generator = gen_special_random_cell_generator(probability_of_victim_cell, probability_of_predator_cell, probability_of_wall_cell)
+        special_random_cell_generator = gen_special_random_cell_generator(
+            probability_victim_cell,
+            probability_predator_cell,
+            probability_wall_cell)
 
         self.field = [[
-            LifeModel.Cell(special_random_cell_generator(), time_to_death, time_to_reproduction_for_victims, time_to_reproduction_for_predators)
+            LifeModel.Cell(
+                special_random_cell_generator(),
+                time_to_death,
+                time_to_reproduction_victims,
+                time_to_reproduction_predators)
             for column in range(column_number)]
             for row in range(row_number)]
 
@@ -113,13 +126,13 @@ class LifeModel(object):
         return 0 <= row < self.row_number and 0 <= column < self.column_number
 
     def __choose_random_direction(self, available_directions):
-        direction = random.randint(0, len(available_directions) - 1)
+        direction = random.randint(LifeModel.LEFT_SUITABLE_INDEX, len(available_directions) - 1)
         row, column = available_directions[direction][0], available_directions[direction][1]
         available_directions.pop(direction)
         return row, column
 
     def __move_from(self, row, column, available_directions):
-        to_row, to_column= self.__choose_random_direction(available_directions)
+        to_row, to_column = self.__choose_random_direction(available_directions)
         self.field[to_row][to_column] = copy(self.field[row][column])
         self.field[row][column].change_cell_type(0)
 
@@ -134,38 +147,50 @@ class LifeModel(object):
                     stats[current_cell_type].append((x, y))
         return stats
 
-    def generate_next_turn(self):
-        for i in range(self.row_number):
-            for j in range(self.column_number):
-                if not self.field[i][j].used:
-                    self.field[i][j].used = True
-                    k = self.field[i][j].cell_type
-                    if k in LifeModel.LIVING_CELLS_TYPES:
-                        stats = self.__get_stats(i, j)
-                        # stats about free spaces and victims nearly
-
-                        if not self.field[i][j].time_to_reproduction and len(stats[0]):
-                            # random replication if free space exists
-                            ch_x, ch_y = self.__choose_random_direction(stats[0])
-                            self.field[ch_x][ch_y].change_cell_type(k)
-                            self.field[i][j].reset_time_to_reproduction()
-
-                        if k == 2 and len(stats[1]):
-                            # eat victim if can
-                            self.field[i][j].time_to_death = self.time_to_death
-                            self.__move_from(i, j, stats[1])
-
-                        if random.random() < self.p_vict_move and len(stats[0]):
-                            # move to near free cell randomly
-                            self.__move_from(i, j, stats[0])
-        self.life_counter = LifeModel.DEFAULT_LIFE_COUNTER
+    def __update_field_state(self):
+        self.life_counter = copy(LifeModel.DEFAULT_LIFE_COUNTER)
         for row in range(self.row_number):
             for column in range(self.column_number):
                 self.field[row][column].update_cell_state()
-                k = self.field[row][column].cell_type
-                if k in LifeModel.LIVING_CELLS_TYPES:
-                    self.life_counter[k] += 1
+                current_cell_type = self.field[row][column].cell_type
+                if current_cell_type in LifeModel.LIVING_CELLS_TYPES:
+                    self.life_counter[current_cell_type] += 1
 
+    def generate_next_turn(self):
+        for row in range(self.row_number):
+            for column in range(self.column_number):
+                current_cell = self.field[row][column]
+                if not current_cell.used:
+                    current_cell.used = True
+                    current_cell_type = current_cell.cell_type
+                    if current_cell_type in LifeModel.LIVING_CELLS_TYPES:
+                        stats = self.__get_stats(row, column)
+                        '''stats about free spaces and victims nearly'''
+
+                        if not current_cell.time_to_reproduction and\
+                                len(stats[LifeModel.CellType.FREE_SPACE]):
+                            '''random replication if free space exists'''
+                            copy_to_x, copy_to_y = self.__choose_random_direction(
+                                                        stats[LifeModel.CellType.FREE_SPACE])
+                            self.field[copy_to_x][copy_to_y].change_cell_type(current_cell_type)
+                            current_cell.reset_time_to_reproduction()
+
+                        if current_cell_type == LifeModel.CellType.PREDATOR and\
+                                len(stats[LifeModel.CellType.VICTIM]):
+                            '''eat victim if can'''
+                            current_cell.time_to_death = self.time_to_death
+                            self.__move_from(row, column, stats[LifeModel.CellType.VICTIM])
+
+                        if len(stats[LifeModel.CellType.FREE_SPACE]):
+                            random_outcome = random.random()
+                            if (current_cell_type == LifeModel.CellType.VICTIM and
+                                random_outcome < self.probability_victim_move) or\
+                                    (current_cell_type == LifeModel.CellType.PREDATOR and
+                                        random_outcome < self.probability_predator_move):
+                                '''move to near free cell randomly'''
+                                self.__move_from(row, column, stats[LifeModel.CellType.FREE_SPACE])
+
+        self.__update_field_state()
         self.__visualize(self.step)
         self.step += 1
 
