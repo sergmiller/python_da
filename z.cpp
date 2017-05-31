ec95172124d48f4d92db4508c91c55433c60b114
sergmiller <sergey.miller@phystech.edu>
2017-05-31 19:14:56 +0300

    add all

--- z.cpp
+++ z.cpp
@@ -31,49 +31,15 @@ using namespace std;
 
 /*@Sergey_Miller*/
 
-ll n;
-ll a[301];
-ll dp[301][301][2];
-ll fl[301][301][2];
-
-ll get(ll l, ll r, ll ind) {
-    if(l == r)
-        return 0;
-    if(ind != -1) {
-        if(fl[l][r][ind]) {
-            return dp[l][r][ind];
-        } else {
-            fl[l][r][ind] = 1;
-        }
-    }
-
-    ll best = 0;
-    ll bnd;
-    if(!ind)
-        bnd = a[l-1];
-    else
-        bnd = a[r];
-    for(ll i = l; i < r; ++i) {
-        if(ind == -1 || a[i] < bnd) {
-            ll lh = get(l,i,1);
-            ll rh = get(i+1,r,0);
-            best = max(best, max(lh, rh) + 1);
-        }
-    }
-    return dp[l][r][ind] = best;
-}
 
 void solve() {
-    cin >> n;
-    loop(i,n)
-        cin >> a[i];
-    cout << get(0,n,-1);
+
 }
 
 
 int main () {
     ios::sync_with_stdio(false);
-    // freopen("out", "r", stdin);
+    freopen("input.txt", "r", stdin);
     // freopen("output.txt", "w", stdout);
 
     solve();
